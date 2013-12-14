1ab6ff3c572dfacc3431156fed698fc0969e7d59
Simon Hohberg <simhoh@gmx.de>
2013-12-14 18:05:17 +0100

    adding custom WholeFileInputFormat

--- HadoopLib/src/hadoopml/libfileinput/WholeFileRecordReader.java
+++ HadoopLib/src/hadoopml/libfileinput/WholeFileRecordReader.java
@@ -1,73 +1,73 @@
 package hadoopml.libfileinput;
 
 import java.io.IOException;
-
+import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-
-public class WholeFileRecordReader extends RecordReader<NullWritable, BytesWritable> {
-
-	private FileSplit split;
-	private Configuration conf;
-	
-	private final BytesWritable currValue = new BytesWritable();
-	private boolean fileProcessed = false;
-	
-	@Override
-	public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
-		this.split = (FileSplit)split;
-		this.conf = context.getConfiguration();
-	}
-	
-	@Override
-	public boolean nextKeyValue() throws IOException, InterruptedException {        
-		if ( fileProcessed ){
-		        return false;
-		}
-		
-		int fileLength = (int)split.getLength();
-		byte [] result = new byte[fileLength];
-		
-		FileSystem  fs = FileSystem.get(conf);
-		FSDataInputStream in = null; 
-		try {
-		        in = fs.open( split.getPath());
-		        IOUtils.readFully(in, result, 0, fileLength);
-		        currValue.set(result, 0, fileLength);
-		        
-		} finally {
-		        IOUtils.closeStream(in);
-		}
-		this.fileProcessed = true;
-		return true;
-	}
-	
-	@Override
-	public NullWritable getCurrentKey() throws IOException, InterruptedException {
-		return NullWritable.get();
-	}
-	
-	@Override
-	public BytesWritable getCurrentValue() throws IOException, InterruptedException {
-		return currValue;
-	}
-	
-	@Override
-	public float getProgress() throws IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	@Override
-	public void close() throws IOException {
-		// nothing to close
-	}
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.mapred.RecordReader;
+ 
+class WholeFileRecordReader implements RecordReader<Text, BytesWritable> {
+  
+    private FileSplit fileSplit;
+    private Configuration conf;
+    private boolean processed = false;
+  
+    public WholeFileRecordReader(FileSplit fileSplit, Configuration conf) throws IOException {
+        this.fileSplit = fileSplit;
+        this.conf = conf;
+    }
+ 
+    @Override
+    public boolean next(Text key, BytesWritable value) throws IOException {
+        if (!processed) {
+            byte[] contents = new byte[(int) fileSplit.getLength()];
+            Path file = fileSplit.getPath();
+ 
+            String fileName = file.getName();
+            key.set(fileName);
+ 
+            FileSystem fs = file.getFileSystem(conf);
+            FSDataInputStream in = null;
+            try {
+                in = fs.open(file);
+                IOUtils.readFully(in, contents, 0, contents.length);                
+                value.set(contents, 0, contents.length);
+            } finally {
+                IOUtils.closeStream(in);
+            }
+            processed = true;
+            return true;
+        }
+        return false;
+    }
+ 
+    @Override
+    public Text createKey() {
+        return new Text();
+    }
+ 
+    @Override
+    public BytesWritable createValue() {
+        return new BytesWritable();
+    }
+ 
+    @Override
+    public long getPos() throws IOException {
+        return processed ? fileSplit.getLength() : 0;
+    }
+ 
+    @Override
+    public float getProgress() throws IOException {
+        return processed ? 1.0f : 0.0f;
+    }
+ 
+    @Override
+    public void close() throws IOException {
+        // do nothing
+    }
 }
