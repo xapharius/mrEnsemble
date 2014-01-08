3260c7774dffcfe12fc6f0e8becdce4f9303d682
Simon Hohberg <simhoh@gmx.de>
2014-01-08 14:14:35 +0100

    minor improvements for readers

--- Engine/src/reader/ImageReader.py
+++ Engine/src/reader/ImageReader.py
@@ -1,7 +1,21 @@
 from reader import BaseReader
+from PIL import Image
 
 class ImageReader(BaseReader):
     
+    def get_data(self):
+        if self.data is None:
+            raise Exception('No data source has been set!')
+        return self.data
+    
+    def set_data_source(self, csv_source):
+        self.data = Image.open()
+
+    def send_to_hdfs(self):
+        pass
+
+    def get_input_protocol(self):
+        pass
+    
     def get_input_format(self):
-        # TODO implement WholeFileInputFormat
         return 'org.apache.hadoop.mapred.WholeFileInputFormat'
