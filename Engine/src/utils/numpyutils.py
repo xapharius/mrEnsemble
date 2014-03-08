c7a9545dcba0c0d210ab78919c48630323c83867
xapharius <mihai.suteu@xapharius.com>
2014-03-08 18:57:37 +0100

    implmented sigmoidScalar, sigmoidNPArray

--- Engine/src/utils/numpyutils.py
+++ Engine/src/utils/numpyutils.py
@@ -4,8 +4,9 @@ Created on Mar 6, 2014
 @author: xapharius
 '''
 import numpy as np
+import math
 
-def addOneToVec(nparr):
+def addOneToVec(npArr):
     '''
     add a one to row or column vec(array)
     @param nparr: np.Array
@@ -14,17 +15,34 @@ def addOneToVec(nparr):
     '''
     
     # nparr is a (one dimensional) vector 
-    if len(nparr.shape) == 1:
-        t_nparr = np.append(nparr, 1)
-        t_nparr = np.reshape(t_nparr, (1,len(t_nparr)))
+    if len(npArr.shape) == 1:
+        t_npArr = np.append(npArr, 1)
+        t_npArr = np.reshape(t_npArr, (1,len(t_npArr)))
     # row vector (array)
-    elif nparr.shape[0] == 1:   
-        t_nparr = np.append(nparr, [[1]], 1)
+    elif npArr.shape[0] == 1:   
+        t_npArr = np.append(npArr, [[1]], 1)
     # column vector (array)
-    elif nparr.shape[1] == 1:
-        t_nparr = np.append(nparr, [[1]], 0)
+    elif npArr.shape[1] == 1:
+        t_npArr = np.append(npArr, [[1]], 0)
     else:
         raise Exception("addOne works only for one dimensional vectors")
         
-    return t_nparr
+    return t_npArr
     
+    
+def sigmoidScalar(x):
+    '''
+    @param x: scalar
+    @return: sigmoid of x
+    @rtype: scalar
+    '''
+    return 1 / (1 + math.exp(-x))
+
+def sigmoidNPArray(npArr):
+    '''
+    @param x: scalar
+    @return: sigmoid of x
+    @rtype: scalar
+    '''
+    func = np.vectorize(sigmoidScalar)
+    return func(npArr)
