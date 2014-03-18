fixing precision issue in numerical pre-processor's statistics, improving according test

Engine/src/datahandler/numerical/numerical_pre_processor.py
@@ -40,12 +40,11 @@ class NumericalPreProcessor(AbstractPreProcessor):
             else:
                 mins = np.vstack((result[self.MIN], _min))
                 maxs = np.vstack((result[self.MAX], _max))
-                means = np.vstack((result[self.MEAN], mean))
                 
                 result[self.MIN] = np.min(mins, axis=0)
                 result[self.MAX] = np.max(maxs, axis=0)
-                result[self.MEAN] = np.mean(means, axis=0)
                 result[self.VAR] = (result[self.VAR]*result[self.NUM] + var*num)/(result[self.NUM] + num) + result[self.NUM]*num*np.power((mean-result[self.MEAN])/(num+result[self.NUM]), 2)
+                result[self.MEAN] = (result[self.MEAN]*result[self.NUM] + mean*num)/(result[self.NUM] + num)
                 result[self.NUM] += num
         sys.stderr.write('aggregate result: ' + str(result) + '\n')
         result[self.MIN] = result[self.MIN].tolist()

Engine/src/tests/datahandler/numerical/numerical_pre_processor_test.py
@@ -33,28 +33,31 @@ class NumericalPreProcessorTest(unittest.TestCase):
 
     def testAggregation(self):
         pre_processor = NumericalPreProcessor()
-        data_set_1 = NumericalDataSet(np.random.random_integers(1,10, (10,10)), np.random.random_integers(1,10, (10,1)))
-        data_set_2 = NumericalDataSet(np.random.random_integers(1,10, (10,10)), np.random.random_integers(1,10, (10,1)))
-        
-        stats_1 = pre_processor.calculate(data_set_1)
-        stats_2 = pre_processor.calculate(data_set_2)
-        
-        result = pre_processor.aggregate(1, [stats_1, stats_2])
-        
-        data = np.vstack((data_set_1.inputs, data_set_2.inputs))
-        num = data.shape[0]
-        _min = np.min(data, axis=0)
-        _max = np.max(data, axis=0)
-        mean = np.mean(data, axis=0)
-        var = np.var(data, axis=0)
-        
-        # TODO: variance accuracy issue
-        tolerance = 2.0
-        self.assertTrue(result[NumericalPreProcessor.NUM] == num, 'num doesn\'t match!\nex: ' + str(num) + '\nis: ' + str(result[NumericalPreProcessor.NUM]))
-        self.assertTrue(all(result[NumericalPreProcessor.MIN] == _min), 'min doesn\'t match!\nex: ' + str(_min) + '\nis: ' + str(result[NumericalPreProcessor.MIN]))
-        self.assertTrue(all(result[NumericalPreProcessor.MAX] == _max), 'max doesn\'t match!\nex: ' + str(_max) + '\nis: ' + str(result[NumericalPreProcessor.MAX]))
-        self.assertTrue(equals_with_tolerance(result[NumericalPreProcessor.MEAN], mean, tolerance), 'mean doesn\'t match!\nex: ' + str(mean) + '\nis: ' + str(result[NumericalPreProcessor.MEAN]))
-        self.assertTrue(equals_with_tolerance(result[NumericalPreProcessor.VAR], var, tolerance), 'variance doesn\'t match!\nex: ' + str(var) + '\nis: ' + str(result[NumericalPreProcessor.VAR]))
+        tolerance = 0.0001
+        for _ in range(200):
+            data_1 = np.random.random_sample((np.random.randint(1, 20), 10))*10.0
+            data_2 = np.random.random_sample((np.random.randint(1, 20), 10))*10.0
+            data_set_1 = NumericalDataSet(data_1, np.random.random_sample((data_1.shape[0],1)))
+            data_set_2 = NumericalDataSet(data_2, np.random.random_sample((data_2.shape[0],1)))
+            
+            stats_1 = pre_processor.calculate(data_set_1)
+            stats_2 = pre_processor.calculate(data_set_2)
+            
+            result = pre_processor.aggregate(1, [stats_1, stats_2])
+            
+            data = np.vstack((data_set_1.inputs, data_set_2.inputs))
+            num = data.shape[0]
+            _min = np.min(data, axis=0)
+            _max = np.max(data, axis=0)
+            mean = np.mean(data, axis=0)
+            var = np.var(data, axis=0)
+            
+            # TODO: variance accuracy issue
+            self.assertTrue(result[NumericalPreProcessor.NUM] == num, 'num doesn\'t match!\nex: ' + str(num) + '\nis: ' + str(result[NumericalPreProcessor.NUM]))
+            self.assertTrue(all(result[NumericalPreProcessor.MIN] == _min), 'min doesn\'t match!\nex: ' + str(_min) + '\nis: ' + str(result[NumericalPreProcessor.MIN]))
+            self.assertTrue(all(result[NumericalPreProcessor.MAX] == _max), 'max doesn\'t match!\nex: ' + str(_max) + '\nis: ' + str(result[NumericalPreProcessor.MAX]))
+            self.assertTrue(equals_with_tolerance(result[NumericalPreProcessor.MEAN], mean, tolerance), 'mean doesn\'t match!\nex: ' + str(mean) + '\nis: ' + str(result[NumericalPreProcessor.MEAN]))
+            self.assertTrue(equals_with_tolerance(result[NumericalPreProcessor.VAR], var, tolerance), 'variance doesn\'t match!\nex: ' + str(var) + '\nis: ' + str(result[NumericalPreProcessor.VAR]))
 
 
 if __name__ == "__main__":